b98e01521107924c2a09bc9507a2ba15c8596c5a
carlosnunesjr <carlosnunes@gmail.com>
2022-06-06 23:17:17 +0000

    Add Swagger documentation

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -9,7 +9,6 @@ class ListAllUsersController {
   handle(request: Request, response: Response): Response {
     let { user_id } = request.headers;
     user_id = user_id as string;
-
     let listUsers = null;
     try {
       listUsers = this.listAllUsersUseCase.execute({ user_id });
